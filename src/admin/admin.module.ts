281cfa1bb3e795c7abb61582d3b665a2967b5c84
Macsoftechnologies <macsoftechnologies@gmail.com>
2022-01-05 17:35:41 +0530

    Get api's added

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,11 +3,14 @@ import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { admin, adminSchema } from './schema/admin.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { VehicleService } from 'src/vehicle/vehicle.service';
+import { vehicle, vehicleSchema } from 'src/vehicle/schema/vehicle.schema';
 
 @Module({
   imports:[MongooseModule.forFeature([{name :admin.name , schema : adminSchema}]),
-],
+  MongooseModule.forFeature([{name :vehicle.name , schema : vehicleSchema}]),
+  ], 
   controllers: [AdminController],
-  providers: [AdminService]
+  providers: [AdminService, VehicleService]
 })
 export class AdminModule {}
