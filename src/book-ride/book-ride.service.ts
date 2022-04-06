30025ded39daa73820934b2672c0b0c0174aef62
Macsoftechnologies <macsoftechnologies@gmail.com>
2022-04-06 16:33:31 +0530

    few changes added

--- src/book-ride/book-ride.service.ts
+++ src/book-ride/book-ride.service.ts
@@ -66,6 +66,8 @@ export class BookRideService {
                         brandsList: brands 
                     }
     
+
+                    
                 }
             }
             return {
