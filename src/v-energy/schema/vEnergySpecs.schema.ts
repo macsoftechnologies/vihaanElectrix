b2bc8d696b7da425b9dccbb4b0908614853f580c
Macsoftechnologies <macsoftechnologies@gmail.com>
2022-07-07 12:17:36 +0530

    charger station

--- src/v-energy/schema/vEnergySpecs.schema.ts
+++ src/v-energy/schema/vEnergySpecs.schema.ts
@@ -1,26 +1,23 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
-import {v4 as uuid} from 'uuid'
-export class loc{
-    longitude: string;
-    latitude: string
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { v4 as uuid } from 'uuid';
+export class loc {
+  longitude: string;
+  latitude: string;
 }
-@Schema({timestamps:true})
-export  class vEnergySpecs{
-    @Prop({required: true, unique:true, default: uuid})
-    chargerId: string
-    @Prop()
-    powerSpecifications: []
-    @Prop()
-    chargerImage: string
-    @Prop()
-    otherSpecifications: []
-    // @Prop()
-    // longitude: string;
-    // @Prop()
-    // latitude: string;
-    @Prop()
-    location: [longitude: string,
-    latitude: string]
+@Schema({ timestamps: true })
+export class vEnergySpecs {
+  @Prop({ required: true, unique: true, default: uuid })
+  chargerId: string;
+  @Prop()
+  powerSpecifications: [];
+  @Prop()
+  chargerImage: string;
+  @Prop()
+  otherSpecifications: [];
+  @Prop()
+  longitude: string;
+  @Prop()
+  latitude: string;
 }
 
-export const  vEnergySpecsSchema = SchemaFactory.createForClass(vEnergySpecs);
+export const vEnergySpecsSchema = SchemaFactory.createForClass(vEnergySpecs);
