a45408e58f8214c153ae149cde2f0f1b64819b97
Macsoftechnologies <macsoftechnologies@gmail.com>
2022-07-29 12:47:02 +0530

    image api added

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, HttpStatus, Post, Query, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Query,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { ImageService } from './image.service';
 import { diskStorage } from 'multer';
@@ -10,68 +19,83 @@ export class ImageController {
 
   @Post('/addImage')
   @UseInterceptors(
-      AnyFilesInterceptor({
-          storage: diskStorage({
-              destination: './files',
-              filename: (req, file, cb) => {
-                  const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
-                  cb(null, `${randomName}${extname(file.originalname)}`)
-              }
-          }),
+    AnyFilesInterceptor({
+      storage: diskStorage({
+        destination: './files',
+        filename: (req, file, cb) => {
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+          cb(null, `${randomName}${extname(file.originalname)}`);
+        },
       }),
+    }),
   )
   async bookRide(@Body() req: imageDto, @UploadedFiles() image) {
-      try {
-          const result = await this.imageService.addImage(req, image)
-          console.log("result", result);
+    try {
+      const result = await this.imageService.addImage(req, image);
+      console.log('result', result);
 
-          return result
-      } catch (error) {
-          return {
-              statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-              message: error.message,
-          };
-      }
+      return result;
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
+    }
   }
 
-  @Get('/getImageList') 
-      async listOfImages() {
-          console.log()
-          try {
-              const response = await this.imageService.imageList()
-              return response
-          } catch (error) {
-              return {
-                  StatusCode : HttpStatus.INTERNAL_SERVER_ERROR,
-                  Message : error
-              }
-          }
-      }
-      @Post('/removeImage')
-      async deleteStore(@Body() req: imageDto){
-          try{
-              let response = await this.imageService.delete(req);
-              return response
-          }
-          catch(error){
-              return{
-                  statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                  message:error.message
-              }
-          }
-          }
-          @Post('/getImage')
-          async find(@Body()  req:imageDto){
-          //console.log('storeName')
-          try{
-           const response = await this.imageService.findImage(req)
-           return response
-           }
-           catch(error){
-           return{
-               StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-               Message: error
-           }
-       }
-    }  
+  @Get('/getImageList')
+  async listOfImages() {
+    console.log();
+    try {
+      const response = await this.imageService.imageList();
+      return response;
+    } catch (error) {
+      return {
+        StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        Message: error,
+      };
+    }
+  }
+  @Post('/removeImage')
+  async deleteStore(@Body() req: imageDto) {
+    try {
+      let response = await this.imageService.delete(req);
+      return response;
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
+    }
+  }
+  @Post('/getImage')
+  async find(@Body() req: imageDto) {
+    //console.log('storeName')
+    try {
+      const response = await this.imageService.findImage(req);
+      return response;
+    } catch (error) {
+      return {
+        StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        Message: error,
+      };
+    }
+  }
+
+  @Post('/getImageByUrl')
+  async findImage(@Body() req: imageDto) {
+    //console.log('storeName')
+    try {
+      const response = await this.imageService.findImageUrl(req);
+      return response;
+    } catch (error) {
+      return {
+        StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        Message: error,
+      };
+    }
+  }
 }
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,110 +1,141 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { image } from './schema/image.schema';
-import { Model } from 'mongoose'
+import { Model } from 'mongoose';
 import { imageDto } from './dto/image.dto';
 @Injectable()
 export class ImageService {
-    
-    constructor(@InjectModel(image.name) private imageModel: Model<image>){}
-    async addImage(req: imageDto, image) {
-        try {
-            console.log(req, "documents...", image)
-            if (image) {
-                const reqDoc = image.map((doc, index) => {
-                    let IsPrimary = false
-                    if (index == 0) {
-                        IsPrimary = true
-                    }
-                    const randomNumber = Math.floor((Math.random() * 1000000) + 1);
-                    return doc.filename
-                })
+  constructor(@InjectModel(image.name) private imageModel: Model<image>) {}
+  async addImage(req: imageDto, image) {
+    try {
+      console.log(req, 'documents...', image);
+      if (image) {
+        const reqDoc = image.map((doc, index) => {
+          let IsPrimary = false;
+          if (index == 0) {
+            IsPrimary = true;
+          }
+          const randomNumber = Math.floor(Math.random() * 1000000 + 1);
+          return doc.filename;
+        });
 
-                req.image = reqDoc.toString()
-            }
-            console.log(req);
-            // return false;
-            const addImageResp = await this.imageModel.create(req)
-            const Count = await this.imageModel.find().count();
-            if (addImageResp) {
-                return {
-                    statusCode: HttpStatus.OK,
-                   addImageRes: addImageResp,
-                   count: Count
-                }
-            }
-            return {
-                statusCode: HttpStatus.BAD_REQUEST,
-                message: "Invalid Request"
-            }
-        } catch (error) {
-            return {
-                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                message: error.message,
-            };
-        }
+        req.image = reqDoc.toString();
+      }
+      console.log(req);
+      // return false;
+      const addImageResp = await this.imageModel.create(req);
+      const Count = await this.imageModel.find().count();
+      if (addImageResp) {
+        return {
+          statusCode: HttpStatus.OK,
+          addImageRes: addImageResp,
+          count: Count,
+        };
+      }
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Invalid Request',
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
     }
-    async imageList() {
-        try {
-    
-            const imagesResp = await this.imageModel.find()
-            console.log(imagesResp)
-            if (imagesResp) {
-                return {
-                    StatusCode: HttpStatus.OK,
-                    imageRes: imagesResp
-                }
-            }
-            return {
-                StatusCode: HttpStatus.BAD_REQUEST,
-                Message: "InValid Request"
-            }
-    
-        } catch (error) {
-            return {
-                StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                Message: error
-    
-            }
-        }
+  }
+  async imageList() {
+    try {
+      const imagesResp = await this.imageModel.find();
+      console.log(imagesResp);
+      if (imagesResp) {
+        return {
+          StatusCode: HttpStatus.OK,
+          imageRes: imagesResp,
+        };
+      }
+      return {
+        StatusCode: HttpStatus.BAD_REQUEST,
+        Message: 'InValid Request',
+      };
+    } catch (error) {
+      return {
+        StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        Message: error,
+      };
     }
+  }
 
-    async delete(body: imageDto){
-        try{
-            const deleteRes = await this.imageModel.deleteMany({imageId: body.imageId});
-            console.log(deleteRes, "resp")
-            return{
-                statusCode: HttpStatus.OK,
-                message: 'Store removed successfully'
-            }
-        }catch(error){
-            let error_response= {
-                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                data: null,
-                message: error
-            }
-            return error_response
-        }
+  async delete(body: imageDto) {
+    try {
+      const deleteRes = await this.imageModel.deleteMany({
+        imageId: body.imageId,
+      });
+      console.log(deleteRes, 'resp');
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Store removed successfully',
+      };
+    } catch (error) {
+      let error_response = {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: null,
+        message: error,
+      };
+      return error_response;
     }
-    
-    async findImage(req){
-        try{
-             const imageResponse = await this.imageModel.find({imageId: req.imageId})
-             if(imageResponse){
-                return{
-                    StatusCode: HttpStatus.OK,
-                    imageDetails: imageResponse
-                   }
-            }
-            return{
-                StatusCode: HttpStatus.BAD_REQUEST,
-                Message: "InValid Request"
-            }
-        } catch(error){
-            return{
-                StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                Message: error
-            }
-        }
+  }
+
+  async findImage(req) {
+    try {
+      const imageResponse = await this.imageModel.find({
+        imageId: req.imageId,
+      });
+      const count = await this.imageModel.count();
+      if (imageResponse) {
+        return {
+          StatusCode: HttpStatus.OK,
+          imageDetails: imageResponse,
+          count: count,
+        };
+      }
+      return {
+        StatusCode: HttpStatus.BAD_REQUEST,
+        Message: 'InValid Request',
+      };
+    } catch (error) {
+      return {
+        StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        Message: error,
+      };
     }
+  }
+
+  async findImageUrl(req) {
+    try {
+      const imageResponse = await this.imageModel.find({
+        $and: [{ imageId: req.imageId, url: req.url }],
+      });
+      const count = await this.imageModel
+        .find({
+          $and: [{ imageId: req.imageId, url: req.url }],
+        })
+        .count();
+      if (imageResponse) {
+        return {
+          StatusCode: HttpStatus.OK,
+          imageDetails: imageResponse,
+          count: count,
+        };
+      }
+      return {
+        StatusCode: HttpStatus.BAD_REQUEST,
+        Message: 'InValid Request',
+      };
+    } catch (error) {
+      return {
+        StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        Message: error,
+      };
+    }
+  }
 }
