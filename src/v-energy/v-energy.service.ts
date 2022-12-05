2d438252d4375b2d6010f746c33d726645ad30e6
Macsoftechnologies <macsoftechnologies@gmail.com>
2022-12-05 14:01:42 +0530

    update api added

--- src/v-energy/v-energy.service.ts
+++ src/v-energy/v-energy.service.ts
@@ -8,275 +8,280 @@ import { vEnergySpecs } from './schema/vEnergySpecs.schema';
 
 @Injectable()
 export class VEnergyService {
-    constructor(@InjectModel(vEnergy.name) private vEnergyModel: Model<vEnergy>, @InjectModel(vEnergySpecs.name) private vEnergySpecsModel: Model<vEnergySpecs>) { }
-    async Create(req: vEnergyDto) {
-        try {
+  constructor(
+    @InjectModel(vEnergy.name) private vEnergyModel: Model<vEnergy>,
+    @InjectModel(vEnergySpecs.name)
+    private vEnergySpecsModel: Model<vEnergySpecs>,
+  ) {}
+  async Create(req: vEnergyDto) {
+    try {
+      const registerRes = await this.vEnergyModel.create(req);
+      if (registerRes) {
+        return {
+          statusCode: HttpStatus.OK,
+          message: 'User Registered SuccessFully',
+          data: {
+            Type: req.type,
+            Location: req.location,
+            UID: req.vEnergyId,
+            OwnerName: req.ownerName,
+            Contact: req.contact,
+            Aadhar: req.aadhar,
+            DeviceUserName: req.deviceOwnerName,
+            ContactName: req.contact,
+          },
+        };
+      }
 
-            const registerRes = await this.vEnergyModel.create(req)
-            if (registerRes) {
-                return {
-                    statusCode: HttpStatus.OK,
-                    message: "User Registered SuccessFully",
-                    data: {
-                        Type: req.type,
-                        Location: req.location,
-                        UID: req.vEnergyId,
-                        OwnerName: req.ownerName,
-                        Contact: req.contact,
-                        Aadhar: req.aadhar,
-                        DeviceUserName: req.deviceOwnerName,
-                        ContactName: req.contact
-                    }
-                }
-
-            }
-
-            return {
-                statusCode: HttpStatus.BAD_REQUEST,
-                message: "Invalid Request"
-            }
-
-        } catch (error) {
-            return {
-                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                message: error.message,
-            };
-        }
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Invalid Request',
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
     }
+  }
 
-    async createCharger(req: vEnergySpecsDto, image) {
-        try {
-            console.log(req, "documents...", image)
-            if (image) {
-                const reqDoc = image.map((doc, index) => {
-                    let IsPrimary = false
-                    if (index == 0) {
-                        IsPrimary = true
-                    }
-                    const randomNumber = Math.floor((Math.random() * 1000000) + 1);
-                    return doc.filename
-                })
+  async createCharger(req: vEnergySpecsDto, image) {
+    try {
+      console.log(req, 'documents...', image);
+      if (image) {
+        const reqDoc = image.map((doc, index) => {
+          let IsPrimary = false;
+          if (index == 0) {
+            IsPrimary = true;
+          }
+          const randomNumber = Math.floor(Math.random() * 1000000 + 1);
+          return doc.filename;
+        });
 
-                req.chargerImage = reqDoc.toString()
-            }
-            console.log(req);
-            // return false;
-            const createVehicleResp = await this.vEnergySpecsModel.create(req)
+        req.chargerImage = reqDoc.toString();
+      }
+      console.log(req);
+      // return false;
+      const createVehicleResp = await this.vEnergySpecsModel.create(req);
 
-            if (createVehicleResp) {
-                return {
-                    statusCode: HttpStatus.OK,
-                    //    message: "Registered SuccessFully",
-                    //    data: {
-                    UserRegistration: {
-                        createVehicleRes: createVehicleResp
-                    }
-                    //        }
-                }
-            }
-            return {
-                statusCode: HttpStatus.BAD_REQUEST,
-                message: "Invalid Request"
-            }
-        } catch (error) {
-            return {
-                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                message: error.message,
-            };
-        }
+      if (createVehicleResp) {
+        return {
+          statusCode: HttpStatus.OK,
+          //    message: "Registered SuccessFully",
+          //    data: {
+          UserRegistration: {
+            createVehicleRes: createVehicleResp,
+          },
+          //        }
+        };
+      }
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Invalid Request',
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
     }
+  }
 
-    async updateCharger(req: vEnergySpecsDto, image) {
-        try {
-            console.log(req, "documents...", image)
-            if (image) {
-                const reqDoc = image.map((doc, index) => {
-                    let IsPrimary = false
-                    if (index == 0) {
-                        IsPrimary = true
-                    }
-                    const randomNumber = Math.floor((Math.random() * 1000000) + 1);
-                    return doc.filename
-                })
+  async updateCharger(req: vEnergySpecsDto, image) {
+    try {
+      console.log(req, 'documents...', image);
+      if (image) {
+        const reqDoc = image.map((doc, index) => {
+          let IsPrimary = false;
+          if (index == 0) {
+            IsPrimary = true;
+          }
+          const randomNumber = Math.floor(Math.random() * 1000000 + 1);
+          return doc.filename;
+        });
 
-                req.chargerImage = reqDoc.toString()
-            }
-            console.log(req);
-            // return false;
-            const createVehicleResp = await this.vEnergySpecsModel.updateOne({ chargerId: req.chargerId},{$set:{chargerImage: req.chargerImage, powerSpecifications: req.powerSpecifications, latitude: req.location.latitude, longitude: req.location.longitude}})          
-                
+        req.chargerImage = reqDoc.toString();
+      }
+      console.log(req);
+      // return false;
+      //const createVehicleResp = await this.vEnergySpecsModel.updateOne({ chargerId: req.chargerId},{$set:{chargerImage: req.chargerImage, powerSpecifications: req.powerSpecifications, latitude: req.location.latitude, longitude: req.location.longitude}})
 
-            if (createVehicleResp) {
-                return {
-                    statusCode: HttpStatus.OK,
-                    //    message: "Registered SuccessFully",
-                    //    data: {
-                    UserRegistration: {
-                        createVehicleRes: createVehicleResp
-                    }
-                    //        }
-                }
-            }
-            return {
-                statusCode: HttpStatus.BAD_REQUEST,
-                message: "Invalid Request"
-            }
-        } catch (error) {
-            return {
-                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                message: error.message,
-            };
-        }
+      const createVehicleResp = await this.vEnergySpecsModel.updateOne(
+        { chargerId: req.chargerId },
+        {
+          $set: {
+            chargerImage: req.chargerImage,
+            powerSpecifications: req.powerSpecifications,
+            latitude: req.latitude,
+            longitude: req.longitude,
+          },
+        },
+      );
+
+      if (createVehicleResp) {
+        return {
+          statusCode: HttpStatus.OK,
+          //    message: "Registered SuccessFully",
+          //    data: {
+          UserRegistration: {
+            createVehicleRes: createVehicleResp,
+          },
+          //        }
+        };
+      }
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Invalid Request',
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
     }
+  }
 
-    async usersList() {
-        try {
-
-            const userResponse = await this.vEnergyModel.find()
-            console.log(userResponse)
-            if (userResponse) {
-                return {
-                    StatusCode: HttpStatus.OK,
-                    Message: 'List of Users',
-                    Data: {
-                        UserDetails: userResponse
-                    }
-
-                }
-            }
-            return {
-                StatusCode: HttpStatus.BAD_REQUEST,
-                Message: "InValid Request"
-            }
-
-        } catch (error) {
-            return {
-                StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                Message: error
-
-            }
-        }
+  async usersList() {
+    try {
+      const userResponse = await this.vEnergyModel.find();
+      console.log(userResponse);
+      if (userResponse) {
+        return {
+          StatusCode: HttpStatus.OK,
+          Message: 'List of Users',
+          Data: {
+            UserDetails: userResponse,
+          },
+        };
+      }
+      return {
+        StatusCode: HttpStatus.BAD_REQUEST,
+        Message: 'InValid Request',
+      };
+    } catch (error) {
+      return {
+        StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        Message: error,
+      };
     }
-    async getChargerDetails(req: vEnergySpecsDto) {
-        try {
-            const vehicleResponse = await this.vEnergySpecsModel.find({ chargerId: req.chargerId })
-            if (vehicleResponse) {
-                return {
-                    StatusCode: HttpStatus.OK,
-                    EnergyStationsResponse: vehicleResponse
-                }
-            }
-            return {
-                StatusCode: HttpStatus.BAD_REQUEST,
-                Message: "InValid Request"
-            }
-        } catch (error) {
-            return {
-                StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                Message: error
-            }
-        }
+  }
+  async getChargerDetails(req: vEnergySpecsDto) {
+    try {
+      const vehicleResponse = await this.vEnergySpecsModel.find({
+        chargerId: req.chargerId,
+      });
+      if (vehicleResponse) {
+        return {
+          StatusCode: HttpStatus.OK,
+          EnergyStationsResponse: vehicleResponse,
+        };
+      }
+      return {
+        StatusCode: HttpStatus.BAD_REQUEST,
+        Message: 'InValid Request',
+      };
+    } catch (error) {
+      return {
+        StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        Message: error,
+      };
     }
-    async findVehicle(chargerId: string) {
-        try {
-            const vehicleResponse = await this.vEnergySpecsModel.findOne({ chargerId: chargerId })
-            if (vehicleResponse) {
-                return {
-                    StatusCode: HttpStatus.OK,
-                    //    Message: "Vehicle Details",
-                    //    Data: {
-                    chargerDetails: vehicleResponse
-                    // }
-                }
-            }
-            return {
-                StatusCode: HttpStatus.BAD_REQUEST,
-                Message: "InValid Request"
-            }
-        } catch (error) {
-            return {
-                StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                Message: error
-            }
-        }
+  }
+  async findVehicle(chargerId: string) {
+    try {
+      const vehicleResponse = await this.vEnergySpecsModel.findOne({
+        chargerId: chargerId,
+      });
+      if (vehicleResponse) {
+        return {
+          StatusCode: HttpStatus.OK,
+          //    Message: "Vehicle Details",
+          //    Data: {
+          chargerDetails: vehicleResponse,
+          // }
+        };
+      }
+      return {
+        StatusCode: HttpStatus.BAD_REQUEST,
+        Message: 'InValid Request',
+      };
+    } catch (error) {
+      return {
+        StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        Message: error,
+      };
     }
+  }
 
-
-    async chargerList() {
-        try {
-
-            const userResponse = await this.vEnergySpecsModel.find()
-            console.log(userResponse)
-            if (userResponse) {
-                return {
-                    StatusCode: HttpStatus.OK,
-                    Message: 'List of chargerStations',
-                    Data: {
-                        UserDetails: userResponse
-                    }
-
-                }
-            }
-            return {
-                StatusCode: HttpStatus.BAD_REQUEST,
-                Message: "InValid Request"
-            }
-
-        } catch (error) {
-            return {
-                StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                Message: error
-
-            }
-        }
+  async chargerList() {
+    try {
+      const userResponse = await this.vEnergySpecsModel.find();
+      console.log(userResponse);
+      if (userResponse) {
+        return {
+          StatusCode: HttpStatus.OK,
+          Message: 'List of chargerStations',
+          Data: {
+            UserDetails: userResponse,
+          },
+        };
+      }
+      return {
+        StatusCode: HttpStatus.BAD_REQUEST,
+        Message: 'InValid Request',
+      };
+    } catch (error) {
+      return {
+        StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        Message: error,
+      };
     }
+  }
 
-    async delete(body: vEnergySpecsDto) {
-        try {
+  async delete(body: vEnergySpecsDto) {
+    try {
+      console.log(body);
+      const deleteRes = await this.vEnergySpecsModel.deleteOne({
+        chargerId: body.chargerId,
+      });
+      console.log(deleteRes, 'deleteRes...');
 
-            console.log(body)
-            const deleteRes = await this.vEnergySpecsModel.deleteOne({ chargerId: body.chargerId });
-            console.log(deleteRes, "deleteRes...")
-
-            return {
-                statusCode: HttpStatus.OK,
-                message: 'Vehicle removed successfully',
-            };
-
-        } catch (error) {
-            let error_response = {
-                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                data: null,
-                message: error,
-            };
-            return error_response;
-        }
-
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Vehicle removed successfully',
+      };
+    } catch (error) {
+      let error_response = {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: null,
+        message: error,
+      };
+      return error_response;
     }
+  }
 
-    async docs() {
-        try {
-            const vehicleResp = await this.vEnergySpecsModel.find().count();
-            console.log(vehicleResp, "vehicleResp...")
-    
-            return {
-                    statusCode: HttpStatus.OK,
-                    message: 'Total number of chargerStations',
-                    resp: vehicleResp
-             };
-            
-            // return {
-            //     StatusCode: HttpStatus.BAD_REQUEST,
-            //     Message: "Company deletion Failed"
-            // }
-            
-        } catch (error) {
-            let error_response = {
-                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-                data: null,
-                message: error,
-            };
-            return error_response;
-        }
+  async docs() {
+    try {
+      const vehicleResp = await this.vEnergySpecsModel.find().count();
+      console.log(vehicleResp, 'vehicleResp...');
+
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Total number of chargerStations',
+        resp: vehicleResp,
+      };
+
+      // return {
+      //     StatusCode: HttpStatus.BAD_REQUEST,
+      //     Message: "Company deletion Failed"
+      // }
+    } catch (error) {
+      let error_response = {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: null,
+        message: error,
+      };
+      return error_response;
     }
+  }
 }
